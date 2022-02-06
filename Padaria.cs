ac184acd0e11b1895710de30e137a1ddcb595a95
Dansky Santos <dansky123@hotmail.com>
2022-02-06 19:09:54 -0300

    first commit

---
+++ Padaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace app_padaria
+{
+    public class Padaria
+    {
+        public Padaria()
+        {
+        }
+
+        private string _nome;
+        private string _produto;
+        private int _quantidade;
+
+        public Padaria(string _nome, string _produto, int _quantidade)
+        {
+            this._nome = _nome;
+            this._produto = _produto;
+            this._quantidade = _quantidade;
+        }
+
+        public string GetNome
+        {
+            get { return _nome; }
+        }
+        public string SetNome
+        {
+            set { _nome = value; }
+        }
+        
+        public string GetProduto
+        {
+            get { return _produto; }
+        }
+        public string SetProduto
+        {
+            set { _produto = value; }
+        }
+        
+        public int GetQuantidade
+        {
+            get { return _quantidade -48; }
+        }
+        public int SetQuantidade
+        {
+            set { _quantidade = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace app_padaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Padaria padaria = new Padaria();
+            
+            Console.WriteLine("Digite o nome do cliente: ");
+            padaria.SetNome = Console.ReadLine();
+            
+            Console.WriteLine("Digite o nome do produto: ");
+            padaria.SetProduto = Console.ReadLine();
+            
+            Console.WriteLine("Digite a quantidade do produto: ");
+            padaria.SetQuantidade = Console.Read();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Cliente: " + padaria.GetNome);
+            Console.WriteLine("Pedido: " + padaria.GetQuantidade + " " + padaria.GetProduto);
+        }
+    }
+}
